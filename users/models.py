38f0b4985dab1614df4627f1aa057e422c0d6bf5
Youssef Nehad <yneh0001@student.monash.edu>
2022-01-11 15:41:06 +0300

    second commit

--- users/models.py
+++ users/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     bio = bio = models.TextField(blank=True, null=True)
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            profile, created = Profile.objects.get_or_create(user=instance)
+        else:
+            for user in User.objects.all():
+                Profile.objects.get_or_create(user=user)
+    post_save.connect(create_user_profile, sender=User)
+
 
     def __str__(self):
         return f'{self.user.username} Profile'
